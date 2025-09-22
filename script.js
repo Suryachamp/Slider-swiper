eaea027334796de13a295f474a82714d5a2196bb
Surya Sekhar Prajapati <113052765+Suryachamp@users.noreply.github.com>
2025-09-22 13:57:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 new Swiper(".card-wrapper", {
   loop: true,
   spaceBetween: 30,
-
-  
   autoplay: {
     delay: 2500,
-    disableOnInteraction: false, 
+    disableOnInteraction: false,
   },
-
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
   breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
+    1200: { slidesPerView: 4 },
+    992: { slidesPerView: 3 },
+    768: { slidesPerView: 2 },
+    0: { slidesPerView: 1 },
   },
 });
